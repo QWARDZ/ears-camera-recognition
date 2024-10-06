2a3ac313d0cb58a1e2781e7c157b2a6fac272ff9
bearneza <johnedwardbearnezaa@gmail.com>
2024-10-06 22:18:34 +0800

    cmit

--- app.py
+++ app.py
@@ -847,7 +847,7 @@ class EmployeeAttendance:
 
     # Function para paandaron ang Flask application
     def run(self):
-        self.app.run(host='192.168.210.134', debug=True, port=3000)
+        self.app.run(debug=True, port=3000) #host='192.168.210.134',
 
 # Main function para mag-instantiate sang EmployeeAttendance class kag i-run ang Flask app
 if __name__ == "__main__":
